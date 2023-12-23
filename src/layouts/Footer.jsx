2783df0d86dde242a8b3a400f4ab1e81ea14e6e4
Engraya <ahmadyakubuaya@gmail.com>
2023-12-23 06:17:54 +0100

    Added Navbar Functionalities

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <footer className="rounded-lg mt-10 text-center shadow dark:bg-gray-800 fixed bottom-0 w-full text-white">
+      <div className="w-full mx-auto max-w-screen-xl p-4  md:items-center md:justify-center">
+    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" className="hover:underline">Delectable™</a>. All Rights Reserved.
+    </span>
+  </div>
+</footer>
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin'),
+  ],
+});
