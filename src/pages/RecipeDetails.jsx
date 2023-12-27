377989a13d8c76cea6a12915f40bd2b6ae840046
Engraya <ahmadyakubuaya@gmail.com>
2023-12-27 19:11:01 +0100

    Fixed Some Issues

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -116,7 +116,7 @@ function RecipeDetails() {
         {/* Product info */}
 
           <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-            <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{recipeDetails.title}</h1>
+            <h1 className="text-2xl font-bold tracking-tight sm:text-3xl">{recipeDetails.title}</h1>
           </div>
 
           {/* Options */}
